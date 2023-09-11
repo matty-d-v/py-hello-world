3285f50a6b2dc750a49b07e217df080bf68323cc
Matty Vickery <matty.d.vickery@gmail.com>
2023-09-11 13:10:28 +0100

    No ticket: added some sample code.

--- hello.py
+++ hello.py
@@ -1,2 +1,13 @@
+
 msg = "Hello world!"
 print(msg)
+
+# And now for something more interesting..
+
+def find_first_odd_total_values(inputVals):
+    for val in inputVals:
+        if (inputVals.count(val) % 2) != 0:
+            return (val)
+
+inputVals = [1,2,2,3,3,3,4,3,3,3,2,2,1]
+print(find_first_odd_total_values(inputVals))
